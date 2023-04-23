a4421a7c627f52f22646f1dd9774d6d5f0abdc61
Oscar Diaz de la Rua <oscarernesto0217@gmail.com>
2023-04-23 00:50:56 -0400

    Big fixes in first function

--- Code/equations.py
+++ Code/equations.py
@@ -2,12 +2,13 @@
 import pandas as pd
 import numpy as np
 import random
+import time
+
 def KMeans(danceability, energy, instrumentalness):
     # KMeans
     # algoritrm for k-means clustering
-    danceability = 0.5
-    energy = 0.5
-    instrumentalness = 0.5
+
+    start_time = time.time()
 
  # id,name,artists,danceability,energy,instrumentalness
     # read in the data
@@ -24,24 +25,52 @@ def KMeans(danceability, energy, instrumentalness):
     # set the centroids to be random points from the data
     centroids = X.sample(K, random_state=random.randint(0, 100)).reset_index(drop = True)   
 
- 
-   
-    
     for i in range(iterations):
-    # calculate distance using distance formula, use argmin to find the closest centroid
-        distances = np.sqrt(((X - centroids)**2).sum(axis=1))
-        print(centroids.iloc[0])
-        labels = np.argmin(distances, axis = 0)
-        # update centroids by setting them as the mean of all points assigned to that centroid 
+
+        distances = []
         for j in range(K):
-            # j corresponds to the centriod
-            # TODO: update the centroids 
-            # placeholder causing key error
-            print(j)
-    for i in range(K):
-        print(f'Cluster {j+1}:')
-        print(data[['artist', 'song']].loc[labels == j])
-        print('\n')# number each cluster from one to ten
+            distances.append(np.sqrt(np.sum((X - centroids.iloc[j])**2, axis=1)))
+
+         # Compute the mean of the data points for each centroid to get the new centroid location
+        labels = np.argmin(distances, axis = 0)
+        for j in range(K):
+            centroids.iloc[j] = X.loc[labels == j].mean()
+
+    target = [danceability, energy, instrumentalness]
+
+    #find closest centroid
+
+    distances = np.sqrt(np.sum((centroids - target)**2, axis=1))
+
+    closest_centroid = distances.argmin()
+    
+    #create array with centroid songs
+
+    centroid_songs = data.loc[labels == closest_centroid]
+
+    centroid_data = centroid_songs[['danceability','energy','instrumentalness']]
+
+    second_centroids = centroid_data.sample(K, random_state=random.randint(0, 100)).reset_index(drop = True)
+
+    for i in range(iterations):
+        second_distances = []
+        for j in range(K):
+            second_distances.append(np.sqrt(np.sum((centroid_data - second_centroids.iloc[j])**2, axis=1)))
+
+            # Compute the mean of the data points for each centroid to get the new centroid location
+        second_labels = np.argmin(second_distances, axis = 0)
+        for j in range(K):
+            second_centroids.iloc[j] = centroid_data.loc[second_labels == j].mean()
+        # run k means again
+
+    second_distances = np.sqrt(np.sum((second_centroids - target)**2, axis=1))
+
+    second_closest_centroid = distances.argmin()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"The function took {elapsed_time} seconds to run.")
+    print("Test")
 
 
 
